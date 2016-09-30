0db64a078380da12bb3c74d3212e4c15e0195c4c
John Hammond <johnhammond010@gmail.com>
2016-09-30 00:34:57 -0400

    Automated certificate creation, removed chown bug, and added colorama install as dependency

--- homemade_ctfs/setup.sh
+++ homemade_ctfs/setup.sh
@@ -14,7 +14,7 @@ DATABASE=""
 CONFIGURATION=""
 
 # Internal variables; do not edit.
-DEPENDENCIES="python-pip sqlite3 python-flask python-passlib"
+DEPENDENCIES="python-pip sqlite3 python-flask python-passlib python-colorama"
 SERVER_FILE="server_base.py"
 NEW_SERVER_FILE="server.py"
 SCHEMA_FILE="schema.sql"
@@ -53,11 +53,19 @@ function install_dependencies(){
 function create_certificate(){
 
 	echo "$0: ${GREEN}creating HTTPS certificates...${NC}"
-	openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout $PRIVATEKEY_FILE -out $CERTIFICATE_FILE || panic
+	openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout $PRIVATEKEY_FILE -out $CERTIFICATE_FILE <<EOF || panic
+US
+CT
+New London
+United States Coast Guard Academy
+Cyber Team
+github.com/uscga
+johnhammond010@gmail.com
+EOF
 
 	sed "0,/\$CERTIFICATE_FILE/{s/\$CERTIFICATE_FILE/$CERTIFICATE_FILE/}" $SERVER_FILE > $NEW_SERVER_FILE || panic
 	sed -i "0,/\$PRIVATEKEY_FILE/{s/\$PRIVATEKEY_FILE/$PRIVATEKEY_FILE/}" $NEW_SERVER_FILE || panic
-	
+
 
 }
 
@@ -67,7 +75,7 @@ function create_database(){
 
 	rm -f $DATABASE
 	sqlite3 $DATABASE < $SCHEMA_FILE || panic
-	chown $CURRENT_USER $DATABASE || panic
+	chown $CURRENT_USER $DATABASE
 	sed -i '0,/\$DATABASE/{s/\$DATABASE/'${DATABASE//\//\\/}'/}' $NEW_SERVER_FILE  || panic
 
 }
@@ -88,7 +96,7 @@ function create_new_server(){
 }
 
 function configure_firewall(){
-	
+
 	# Allow incoming connections...
 	echo "$0: ${GREEN} Configuring firewall for HTTPS connections...${NC}"
 	ufw allow https
